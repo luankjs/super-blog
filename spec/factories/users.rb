22815c24c2d12a648aa0abb9e918f478e3ef84aa
Luan Santos <luan@tootz.com.br>
2022-09-20 12:06:50 -0300

    improvement: add password to users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.email }
+    password: { '123123123' }
 
     trait :without_email do
       name { Faker::Name.name }
